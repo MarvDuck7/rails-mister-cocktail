93edbb2346382ae1217bcb722e24fb4e024f65a3
Heritiana Rabary <marvduck7@gmail.com>
2017-05-11 17:29:19 +0200

    DoseController created

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,12 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktail/show"
+    end
+  end
+end

6265bef7262b309c15839ab4fa142eb9e59e5eed
Heritiana Rabary <marvduck7@gmail.com>
2017-05-11 17:08:46 +0200

    Controllers and routes updated

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,17 +5,24 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @restaurant
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def new
-    @cocktails = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def edit
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def update
@@ -23,6 +30,13 @@ class CocktailsController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 
 end
 
